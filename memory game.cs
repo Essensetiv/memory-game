6be13b55f3f218735f583124e65a49991586cef0
influence <163916433+Essensetiv@users.noreply.github.com>
2025-01-20 23:43:16 +0300

    Update memory game.cs

--- memory game.cs
+++ memory game.cs
@@ -1,47 +1,69 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-public class Test
+
+public class MemoryTrainingGame
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        HashSet<int> a = new HashSet<int>();
-        int firstNumber = -1;
-        int correctCount = 0;
-        Console.WriteLine("Добро пожаловать в игру на память");
+        // Инициализация коллекции для хранения введенных чисел
+        HashSet<int> enteredNumbers = new HashSet<int>();
+        int previousNumber = -1; // Переменная для хранения предыдущего введенного числа
+        int correctCount = 0; // Счетчик правильно введенных чисел
+
+        // Приветствие и правила игры
+        Console.WriteLine("Добро пожаловать в игру на память!");
         Console.WriteLine("Правила игры:");
-        Console.WriteLine("1.Диапозон введеннхы чисел от 0 до 255");
-        Console.WriteLine("2.Запрещается вводить последовательно два числа ,разность между которыми меньше 7");
-        Console.WriteLine("3.Запрещается вводить повтолряющиеся числа");
-        Console.WriteLine("4.Игра закончится после первой ошибки ");
+        Console.WriteLine("1. Диапазон введенных чисел от 0 до 255.");
+        Console.WriteLine("2. Запрещается вводить последовательно два числа, разность между которыми меньше 7.");
+        Console.WriteLine("3. Запрещается вводить повторяющиеся числа.");
+        Console.WriteLine("4. Игра закончится после первой ошибки.");
         Console.WriteLine("Начнём!");
+
+        // Основной игровой цикл
         while (true)
         {
-            Console.WriteLine("Введите число: ");
+            Console.Write("Введите число: ");
             string userInput = Console.ReadLine();
-            if (!int.TryParse(userInput, out int UserNumber)) ;
+
+            // Проверка на корректность ввода (целое число)
+            if (!int.TryParse(userInput, out int userNumber))
             {
-                Console.WriteLine("Ошибка: Введите целое число ");
+                Console.WriteLine("Ошибка: Введите целое число.");
                 break;
             }
-            if (UserNumber < 0 || UserNumber > 255)
+
+            // Проверка на диапазон числа
+            if (userNumber < 0 || userNumber > 255)
             {
-                Console.WriteLine("Ошибка: допустимый диапозон от 0 до 255");
+                Console.WriteLine("Ошибка: Допустимый диапазон от 0 до 255.");
                 break;
             }
-            if (a.Contains(UserNumber))
+
+            // Проверка на повторное введение числа
+            if (enteredNumbers.Contains(userNumber))
             {
-                Console.WriteLine("Ошибка: число уже было введено ");
+                Console.WriteLine("Ошибка: Число уже было введено.");
                 break;
             }
-            if (firstNumber != -1 && Math.Abs(UserNumber - firstNumber) < 7) ;
+
+            // Проверка на разность с предыдущим числом
+            if (previousNumber != -1 && Math.Abs(userNumber - previousNumber) < 7)
             {
-                Console.WriteLine("Ошибка: разность чисел меньше 7 ");
+                Console.WriteLine("Ошибка: Разность чисел меньше 7.");
+                break;
             }
-            a.Add(UserNumber);
+
+            // Если все проверки пройдены, добавляем число в коллекцию
+            enteredNumbers.Add(userNumber);
             correctCount++;
-            firstNumber = UserNumber;
-            Console.WriteLine($"Поздравляю! Игра окончена!Количество верных чисел {correctCount}");
+            previousNumber = userNumber;
+
+            Console.WriteLine($"Число {userNumber} принято. Продолжайте!");
         }
+
+        // Завершение игры
+        Console.WriteLine($"Игра окончена! Количество верно введенных чисел: {correctCount}");
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
     }
 }
